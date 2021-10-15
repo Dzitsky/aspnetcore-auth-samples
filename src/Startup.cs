f017b3ebc38490d3b1c86fcf8b7198f6ed3a2462
stop-cran <stop-cran@list.ru>
2021-10-15 16:39:11 +0300

    Authorization handler samples.

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Authorization.Samples.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Authorization.Samples
 {
@@ -35,25 +38,71 @@ namespace Authorization.Samples
                 op.AddPolicy(Policies.RequireTelephoneNumber,
                     new AuthorizationPolicyBuilder(AuthenticationSchemes.Ldap)
                         .RequireAuthenticatedUser()
-                        .RequireClaim(AppClaimTypes.TelephoneNumber)
+                        .AddRequirements(new HasAllAdditionalAttributesRequirement())
                         .Build());
                 op.AddPolicy(Policies.RequireGlobalAdminRealm,
                     new AuthorizationPolicyBuilder(AuthenticationSchemes.Ldap)
                         .RequireAuthenticatedUser()
                         .RequireClaim(AppClaimTypes.AdminRealm, "global")
                         .Build());
-                
+
                 op.AddPolicy(Policies.RequireAge18Plus,
                     new AuthorizationPolicyBuilder(AuthenticationSchemes.Ldap)
                         .RequireAuthenticatedUser()
-                        .RequireAssertion(context =>
-                            int.TryParse(context.User.Claims.FirstOrDefault(c => c.Type == AppClaimTypes.Age)?.Value,
-                                out var i) && i >= 18)
+                        .AddRequirements(new MinAgeRequirement(18))
                         .Build());
             });
 
-
+            services.AddTransient<IAuthorizationHandler, AgeAuthorizationHandler>();
+            services.AddTransient<IAuthorizationHandler, HasAllAdditionalAttributesAuthorizationHandler>();
             services.AddControllers();
+        }
+
+        public class MinAgeRequirement : IAuthorizationRequirement
+        {
+            public MinAgeRequirement(int minAge)
+            {
+                MinAge = minAge;
+            }
+
+            public int MinAge { get; }
+        }
+
+        public class AgeAuthorizationHandler : AuthorizationHandler<MinAgeRequirement>
+        {
+            protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                MinAgeRequirement requirement)
+            {
+                if (int.TryParse(context.User.Claims.FirstOrDefault(c => c.Type == AppClaimTypes.Age)?.Value,
+                    out var age))
+                    if (age >= requirement.MinAge)
+                        context.Succeed(requirement);
+                    else
+                        context.Fail();
+            }
+        }
+
+        public class HasAllAdditionalAttributesRequirement : IAuthorizationRequirement
+        {
+        }
+
+        public class
+            HasAllAdditionalAttributesAuthorizationHandler : AuthorizationHandler<HasAllAdditionalAttributesRequirement>
+        {
+            private IReadOnlyList<string> attributes;
+
+            public HasAllAdditionalAttributesAuthorizationHandler(IOptions<LdapAuthenticationOptions> options)
+            {
+                attributes = options.Value.AdditionalClaimAttributes;
+            }
+
+            protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                HasAllAdditionalAttributesRequirement requirement)
+            {
+                if (context.User.Claims.Join(attributes, c => c.Type, c => c, (a, b) => true).Count() ==
+                    attributes.Count)
+                    context.Succeed(requirement);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
